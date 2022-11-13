cc691c654b41e425bfce9c7f9121de1e90cc5ab2
Anurag301 <115408889+Anurag301@users.noreply.github.com>
2022-11-13 21:02:10 +0530

    Add files via upload

---
+++ assignment part1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Point:
+    def __init__(self,x,y,z):
+        self.x=x
+        self.y=y
+        self.z=z
+    def SqSum(self):
+        self.sqx=self.x**2
+        self.sqy=self.y**2
+        self.sqz=self.z**2
+        return self.sqx+self.sqy+self.sqz
+a=Point(1,3,5)
+print(a.SqSum())
+
+
+# In[ ]:
+
+
+
+
